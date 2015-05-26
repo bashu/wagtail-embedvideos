dafb474b331bd7909f835b7c3caa5efde731c5a3
Diogo Marques <DiogoMarques29@users.noreply.github.com>
2015-05-26 10:30:30 +0100

    Update wagtail_hooks.py
    Fixed injection of js file for creating the embed video chooser

--- wagtail_embed_videos/wagtail_hooks.py
+++ wagtail_embed_videos/wagtail_hooks.py
@@ -24,16 +24,37 @@ def construct_main_menu(request, menu_items):
     pass
 
 
-class EmbedVideosMenuItem(MenuItem):
-    def is_shown(self, request):
-        return request.user.has_perm('wagtail_embed_videos.add_video')
 
 @hooks.register('register_admin_menu_item')
 def register_embed_videos_menu_item():
-    return EmbedVideosMenuItem(_('Embed Videos'), urlresolvers.reverse('wagtail_embed_videos_index'), classnames='icon icon-media', order=301)
+    return EmbedVideosMenuItem(
+        _('Embed Videos'),
+        urlresolvers.reverse('wagtail_embed_videos_index'),
+        classnames='icon icon-media', order=301)
+
+
+@hooks.register('insert_editor_js')
+def editor_js():
+    js_files = [
+        'wagtail_embed_videos/js/embed-video-chooser.js',
+    ]
+    js_includes = format_html_join('\n', '<script src="{0}{1}"></script>', (
+        (settings.STATIC_URL, filename) for filename in js_files)
+    )
+    return js_includes + format_html(
+        """
+        <script>
+            window.chooserUrls.embedVideoChooser = '{0}';
+        </script>
+        """,
+        urlresolvers.reverse('wagtail_embed_videos_chooser')
+    )
+
 
 @hooks.register('register_permissions')
 def register_permissions():
-    embed_video_content_type = ContentType.objects.get(app_label='wagtail_embed_videos', model='embedvideo')
-    embed_video_permissions = Permission.objects.filter(content_type = embed_video_content_type)
+    embed_video_content_type = ContentType.objects.get(
+        app_label='wagtail_embed_videos', model='embedvideo')
+    embed_video_permissions = Permission.objects.filter(
+        content_type=embed_video_content_type)
     return embed_video_permissions
