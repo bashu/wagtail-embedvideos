667807c6d7232945e6ca29d889adf24200b953b5
José Alaguna <alagunasalahaddin@live.com>
2017-03-05 17:12:23 +0300

    :fire: Modified verbose name application.

--- wagtail_embed_videos/apps.py
+++ wagtail_embed_videos/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class WagtailEmbedVideosAppConfig(AppConfig):
     name = 'wagtail_embed_videos'
     label = 'wagtail_embed_videos'
-    verbose_name = "Wagtail embed videos"
+    verbose_name = "Wagtail Embed Video"
