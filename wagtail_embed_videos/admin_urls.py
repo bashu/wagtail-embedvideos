72af913c96f7a5dd5bfe8cdf5da8f41f94dd785b
Basil Shubin <basil.shubin@gmail.com>
2021-11-07 16:35:19 +0700

    Compatibility fixes for Wagtail >= 2.10

--- wagtail_embed_videos/admin_urls.py
+++ wagtail_embed_videos/admin_urls.py
@@ -1,16 +1,16 @@
-from django.conf.urls import url
+from django.urls import path
 
 from wagtail_embed_videos.views import chooser, embed_videos
 
 app_name = "wagtail_embed_videos"
 
 urlpatterns = [
-    url(r"^$", embed_videos.index, name="index"),
-    url(r"^(\d+)/$", embed_videos.edit, name="edit"),
-    url(r"^(\d+)/delete/$", embed_videos.delete, name="delete"),
-    url(r"^add/$", embed_videos.add, name="add"),
-    url(r"^usage/(\d+)/$", embed_videos.usage, name="embed_video_usage"),
-    url(r"^chooser/$", chooser.chooser, name="chooser"),
-    url(r"^chooser/(\d+)/$", chooser.embed_video_chosen, name="embed_video_chosen"),
-    url(r"^chooser/upload/$", chooser.chooser_upload, name="chooser_upload"),
+    path("", embed_videos.index, name="index"),
+    path("<int:embed_video_id>/", embed_videos.edit, name="edit"),
+    path("<int:embed_video_id>/delete/", embed_videos.delete, name="delete"),
+    path("add/", embed_videos.add, name="add"),
+    path("usage/<int:embed_video_id>/", embed_videos.usage, name="embed_video_usage"),
+    path("chooser/", chooser.chooser, name="chooser"),
+    path("chooser/<int:embed_video_id>/", chooser.embed_video_chosen, name="embed_video_chosen"),
+    path("chooser/upload/", chooser.chooser_upload, name="chooser_upload"),
 ]
